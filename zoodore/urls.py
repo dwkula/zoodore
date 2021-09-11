007eaf2453af304c3b93e0a8b536d00a22df6366
dwkula <dawid.kula98@gmail.com>
2021-09-11 12:40:55 +0200

    first commit

---
+++ zoodore/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('', ),
+    path('admin/', admin.site.urls),
+    path('profile/', include('account.urls'))
+]
